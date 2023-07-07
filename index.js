384c44aee263256bfd8cedd4d4ce3c900a977093
Deekshith Shetty <deekshithshetty698@gmail.com>
2023-07-07 11:56:16 +0530

    Create a seperate file to control the route of endpoints.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const restaurants = require("./api/restaurants.route");
 
 // Configure dotenv files.
 const dotenv = require("dotenv");
@@ -18,6 +19,9 @@ app.use(express.json());
 app.listen(5000, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
+//Request to "/api/restaurants" route will be handled by "restaurants.route.js" file.
+app.use("/api/restaurants", restaurants);
 
 app.use("*", (req, res) => {
   res.status(404).json({ error: "Not Found" });
